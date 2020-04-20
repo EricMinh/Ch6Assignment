b716a8557a57168d5853dc2db552b64de211b75b
EricMinh <32127944+EricMinh@users.noreply.github.com>
2020-04-20 15:34:33 -0700

    Update 6_3.c

--- 6_3.c
+++ 6_3.c
@@ -122,3 +122,37 @@ void treexprint(struct tnode *p) {
 struct linklist *lalloc(void) {
 	return(struct linklist*) malloc(sizeof(struct linklist));
 }
+
+noiseword(char *w) {
+	static char *nw[] = {
+		"a",
+		"an",
+		"and",
+		"are",
+		"in",
+		"is",
+		"of",
+		"or",
+		"that",
+		"the",
+		"this",
+		"to"
+	};
+	int cond, mid;
+	int low = 0;
+	int high = sizeof(nw) / sizeof(char*) - 1;
+
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if ((cond = strcmp(w, nw[mid])) < 0) {
+			high = mid - 1;
+		}
+		else if (cond > 0) {
+			low = mid + 1;
+		}
+		else {
+			return mid;
+		}
+	}
+	return -1;
+}
